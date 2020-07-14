fe13e21fa5a92837fbf28eaa33b9bf5f52abf2cc
Semyon Isaev <semacool1@gmail.com>
2020-07-14 14:04:23 +0300

    Updates

---
+++ precache-manifest.fc76ccc1ab4b44fdfb58a2a5e6f06772.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17de733694169b7a1fdc0f81477f138",
+    "url": "/ReactCoins/index.html"
+  },
+  {
+    "revision": "2fa56b2c6cf87b077222",
+    "url": "/ReactCoins/static/css/main.de605459.chunk.css"
+  },
+  {
+    "revision": "6bee4c3f0dd54ea3dd88",
+    "url": "/ReactCoins/static/js/2.11cb9673.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactCoins/static/js/2.11cb9673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa56b2c6cf87b077222",
+    "url": "/ReactCoins/static/js/main.8712016e.chunk.js"
+  },
+  {
+    "revision": "477225134ed4d3ba90c6",
+    "url": "/ReactCoins/static/js/runtime-main.2ee3f771.js"
+  },
+  {
+    "revision": "792189d69d32520466a68bc6a95bebcb",
+    "url": "/ReactCoins/static/media/blackLogo.792189d6.svg"
+  },
+  {
+    "revision": "72f574eecf50b996ce38046427ee9ba7",
+    "url": "/ReactCoins/static/media/menu.72f574ee.svg"
+  },
+  {
+    "revision": "33315527eafd161649470a27276a7a37",
+    "url": "/ReactCoins/static/media/search.33315527.svg"
+  },
+  {
+    "revision": "cda83765e4258896e04639e49206b6b5",
+    "url": "/ReactCoins/static/media/settings.cda83765.svg"
+  }
+]);
