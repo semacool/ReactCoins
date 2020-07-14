2f8cf0a1ccd11319dda8d914a595b7a0fb4b438e
Isaev Semyon <semacool1@gmail.com>
2020-07-14 13:22:52 +0300

    AddBuild

---
+++ precache-manifest.77a519a4b376fdb74f43606d71334c64.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa5854c507ced9924455c2a31de3422",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30e4792224ec3bdb5409",
+    "url": "/static/css/main.de605459.chunk.css"
+  },
+  {
+    "revision": "6bee4c3f0dd54ea3dd88",
+    "url": "/static/js/2.11cb9673.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.11cb9673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e4792224ec3bdb5409",
+    "url": "/static/js/main.257902d8.chunk.js"
+  },
+  {
+    "revision": "dacb98386bc152e3662b",
+    "url": "/static/js/runtime-main.c6227f45.js"
+  },
+  {
+    "revision": "792189d69d32520466a68bc6a95bebcb",
+    "url": "/static/media/blackLogo.792189d6.svg"
+  },
+  {
+    "revision": "72f574eecf50b996ce38046427ee9ba7",
+    "url": "/static/media/menu.72f574ee.svg"
+  },
+  {
+    "revision": "33315527eafd161649470a27276a7a37",
+    "url": "/static/media/search.33315527.svg"
+  },
+  {
+    "revision": "cda83765e4258896e04639e49206b6b5",
+    "url": "/static/media/settings.cda83765.svg"
+  }
+]);
